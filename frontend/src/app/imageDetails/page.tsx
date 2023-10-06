61bba6b2d60c7b7aa49a648471616662643cab31
Rebeccca Pösche Ruud <rebeccaruuud@gmail.com>
2023-10-06 13:31:06 +0200

    MIA-23: Made the image details page

---
+++ frontend/src/app/imageDetails/page.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import React from "react";
+import { Box, Input } from "@mui/material";
+import Button from "@mui/material/Button";
+import Navbar from "@/components/Navbar";
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      <div  style={{  }}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            m: 2,
+            height: 1000,
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignContent: "stretch",
+              width: "70%",
+              pr: 2,
+            }}
+          >
+            <Box sx={{ bgcolor: "text.disabled", height: "10%", mb: 1 }}>Drawing/Editing tool</Box>
+            <Box sx={{ bgcolor: "text.disabled", height: "90%" }}>Image</Box>
+          </Box>
+          <Box sx={{ bgcolor: "text.disabled", width: "30%" }}>Comments</Box>
+        </Box>
+      </div>
+    </div>
+  );
+}
+
+export default page;
