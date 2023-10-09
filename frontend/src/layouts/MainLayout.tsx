152d109be149d15feee1252c064943c03c05fae6
Saif Warraich <msaif.ib10@gmail.com>
2023-10-09 21:10:12 +0200

    Created Image Editor
    Created Image editor design. It includes:
    - Tool bar
    - Canvas
    - Rectangle, Brush and Eraser functionality.

--- frontend/src/layouts/MainLayout.tsx
+++ frontend/src/layouts/MainLayout.tsx
@@ -1,12 +1,15 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
+import TopBar from "@/components/TopBar/TopBar";
 import { PropsWithChildren } from "react";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div style={{ display: "flex", height: "100vh" }}>
-      <Sidebar />
-      <div>Home</div>
-      {children}
+    <div>
+      <TopBar />
+      <div style={{ display: "flex", height: "100vh" }}>
+        <Sidebar />
+        {children}
+      </div>
     </div>
   );
 };
--- frontend/src/theme/Colors.ts
+++ frontend/src/theme/Colors.ts
@@ -1,6 +1,8 @@
 const colors = {
   chablis: "#FEF3F2",
   isabelline: "#f0ebeb",
+  vistaWhite: "#fcf7f7",
+  razzmatazz: "#e6104d",
 };
 
 export default colors;
