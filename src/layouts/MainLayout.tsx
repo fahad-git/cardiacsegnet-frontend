f29da70a3faefb5fae25051b95c219186b57d900
Rebeccca Pösche Ruud <rebeccaruuud@gmail.com>
2023-11-03 12:17:15 +0100

    Small design changes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,11 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
-import TopBar from "@/components/TopBar/TopBar";
+import NavbarLoggedIn from "@/components/Navigation/NavbarLoggedIn";
 import { PropsWithChildren } from "react";
 
 export const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <div>
-      <TopBar />
+      <NavbarLoggedIn />
       <div style={{ display: "flex", height: "calc(100vh - 42px)" }}>
         <Sidebar />
         {children}
